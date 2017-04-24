35168ea382c8ff591643fc17f38344e9e4caf34c
Jeremy Wicks <artfullu@gmail.com>
2017-04-24 18:17:31 -0400

    Uploading work

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+var port = 3000;
+
+app.listen(port);
+console.log(`App is running on ${port}`);
+
+app.get('/', function(req,res){
+	res.send('Hello my friend!');
+});
+
+
+app.get('/news', function(req,res){
+	res.send('Welcome to the news');
+});
