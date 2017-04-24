82ac788aff9b98868da6bdeed39495e8aaa55b93
Jeremy Wicks <artfullu@gmail.com>
2017-04-24 18:36:30 -0400

    Uploading at part 4

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
+// const volley = require('volleyball');
 var port = 3000;
 
 app.listen(port);
 console.log(`App is running on ${port}`);
 
-app.get('/', function(req,res){
+app.get('/', function(req, res, next){
+	console.log(req.method, req.route.path);
 	res.send('Hello my friend!');
+	next();
+});
+
+app.get('/', function(req, res){
+	console.log(res.statusCode);
 });
 
 
-app.get('/news', function(req,res){
+app.get('/news', function(req, res){
+	console.log(req.method, req.route.path);
 	res.send('Welcome to the news');
+});
+
+app.get('/news', function(req, res){
+	console.log(res.statusCode);
 });
