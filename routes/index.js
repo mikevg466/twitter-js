b9bd1450983cf6dfc2a05a819cc6b83c98a64817
Mike van Genderen <mikevg466>
2017-04-25 11:38:34 -0400

    serving with style

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,20 @@
 
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
+router.use('/', express.static(path.join(__dirname, '/../public')))
 
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
   let tweets = tweetBank.list();
+  // res.render( 'index', { tweets: tweets.map(el => el.content) } );
   res.render( 'index', { tweets: tweets } );
+  next();
 });
+// router.get('/stylesheets/style.css', function(req, res, next){
+//   res.sendFile('style.css'));
+// });
+
 
 module.exports = router;
