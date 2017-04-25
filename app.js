a018998ff600a5bb8a6d0fdbe5991209110020b3
Mike van Genderen <mikevg466>
2017-04-25 10:56:10 -0400

    adds routing

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
+'use strict';
+
 const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
-// const volley = require('volleyball');
+const tweetBack = require('./tweetBank.js');
+const routes = require('./routes');
 var port = 3000;
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name:'Son'}];
+// const people = [{name: 'Full'}, {name: 'Stacker'}, {name:'Son'}];
 
 
 app.listen(port);
 console.log(`App is running on ${port}`);
 
-app.get('/', function(req, res, next){
-	console.log(req.method, req.route.path);
-	res.render('index', {title: 'Hall of Fame', people: people});
-	next();
-});
-
-app.get('/', function(req, res){
-	console.log(res.statusCode);
-});
-
-app.get('/news', function(req, res){
-	console.log(res.statusCode);
-});
-
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
+
+app.use('/', routes);
+
+
+
+// app.get('/', function(req, res, next){
+// 	console.log(req.method, req.route.path);
+// 	res.render('index', {title: 'Hall of Fame', people: people});
+// 	next();
+// });
+//
+// app.get('/', function(req, res){
+// 	console.log(res.statusCode);
+// });
+//
+// app.get('/news', function(req, res){
+// 	console.log(res.statusCode);
+// });
+//
+
 
 // var variables = {
 // title: 'An Example',
